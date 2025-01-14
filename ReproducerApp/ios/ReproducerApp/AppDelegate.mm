b052343a731d0ff1d561e24a1380d49749958db6
chayathon <chayathon@robinhood.co.th>
2025-01-14 10:12:41 +0700

    Update setting to match project

--- ReproducerApp/ios/ReproducerApp/AppDelegate.mm
+++ ReproducerApp/ios/ReproducerApp/AppDelegate.mm
@@ -14,6 +14,36 @@
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+  {
+    [FIRMessaging messaging].APNSToken = deviceToken;
+    // NSString *fcmToken = [FIRMessaging messaging].FCMToken;
+    // NSLog(@"++APNST deviceToken : %@", deviceToken);
+    // NSLog(@"++FCM device token : %@", fcmToken);
+  }
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+  // NSLog(@"!!! ERROR regisgering for APNS : %@", error);
+}
+
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
+
+- (BOOL)application:(UIApplication *)application continueUserActivity:(nonnull NSUserActivity *)userActivity
+ restorationHandler:(nonnull void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler
+{
+ return [RCTLinkingManager application:application
+                  continueUserActivity:userActivity
+                    restorationHandler:restorationHandler];
+}
+
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
   return [self getBundleURL];
